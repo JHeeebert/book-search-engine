a1b1096a45b37050393caffba0610eab269ff85a
Joshua Hebert <jhebertcocktails@gmail.com>
2023-09-27 17:45:35 -0400

    updated server.js

--- server/server.js
+++ server/server.js
@@ -5,11 +5,8 @@ const path = require('path');
 const { ApolloServer } = require('apollo-server-express');
 const { authMiddleware } = require('./utils/auth');
 const { typeDefs, resolvers } = require('./schemas');
-// Add SweetAlert2 
-const Swal = require('sweetalert2');
 // import connection to database and create a new Apollo server
 const db = require('./config/connection');
-const routes = require('./routes');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -21,26 +18,22 @@ const server = new ApolloServer({
 });
 // Apply our middleware to the apollo server
 server.applyMiddleware({ app });
+// Tells the app to use express.json and express.urlencoded to parse incoming data
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 // Serve up static assets
 const _dirname = path.dirname('');
 const pathToFile = path.join(_dirname, '../client/build');
 app.use(express.static(pathToFile));
-
-// wildcard GET route for the server
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
-
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
-
-app.use(routes);
-
+// wildcard GET route for the server
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+// Start the server once the database connection is open
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
   console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
