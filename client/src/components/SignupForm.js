5312cf6224cdbab389b813ffa6c292a0cb05f8f8
Joshua Hebert <jhebertcocktails@gmail.com>
2023-09-27 18:06:14 -0400

    trying to debug signup and login errors

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,9 +1,9 @@
-import React, { useState } from 'react';
-import { Form, Button, Alert } from 'react-bootstrap';
+import React, { useState } from "react";
+import { Form, Button, Alert } from "react-bootstrap";
 
-import { ADD_USER } from '../utils/mutations';
-import Auth from '../utils/auth';
-import { useMutation } from '@apollo/client';
+import Auth from "../utils/auth";
+import { useMutation } from "@apollo/react-hooks";
+import { ADD_USER } from "../utils/mutations";
 
 const SignupForm = () => {
   // set initial form state
