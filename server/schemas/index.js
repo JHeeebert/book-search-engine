99590acc31c3b6c37c751382ddba7d08020b344e
Joshua Hebert <jhebertcocktails@gmail.com>
2023-09-27 17:40:25 -0400

    deleted files and chnaged resolvers.js

--- server/schemas/index.js
+++ server/schemas/index.js
@@ -1,6 +1,4 @@
-// Create typeDefs and resolvers for our schema
 const typeDefs = require("./typeDefs");
 const resolvers = require("./resolvers");
 
-// Export the typeDefs and resolvers
 module.exports = { typeDefs, resolvers };
--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,17 +1,16 @@
-const { User, Book } = require('../models');
-const { AuthenticationError } = require('apollo-server-express');
-const { signToken } = require('../utils/auth');
-// Add SweetAlert2
-const Swal = require('sweetalert2');
+const { User, Book } = require("../models");
+const { AuthenticationError } = require("apollo-server-express");
+const { signToken } = require("../utils/auth");
 
-// Create the functions that fulfill the queries defined in `typeDefs.js`
 const resolvers = {
     Query: {
         me: async (_, __, context) => {
-            if (context.user) {
-                throw new AuthenticationError('You need to be logged in!');
+            if (!context.user) {
+                throw new AuthenticationError("Not logged in");
             }
-            const userData = await User.findById(context.user._id).select('-__v -password');
+            const userData = await User.findById(context.user._id).select(
+                "-__v -password"
+            );
             return userData;
         },
     },
@@ -23,59 +22,41 @@ const resolvers = {
         },
         login: async (_, { email, password }) => {
             const user = await User.findOne({ email });
-            if (!user) {
-                Swal.fire({
-                    icon: 'error',
-                    title: 'Oops...',
-                    text: 'Email not found!',
-                });
-                throw new AuthenticationError('Incorrect credentials');
+
+            if (!user || !(await user.isCorrectPassword(password))) {
+                throw new AuthenticationError("Incorrect credentials");
             }
-            const correctPw = await user.isCorrectPassword(password);
-            if (!correctPw) {
-                Swal.fire({
-                    icon: 'error',
-                    title: 'Oops...',
-                    text: 'Incorrect password!',
-                });
-                throw new AuthenticationError('Incorrect credentials');
-            }
+
             const token = signToken(user);
             return { token, user };
         },
         saveBook: async (_, { book }, context) => {
-            if (context.user) {
-                Swal.fire({
-                    icon: 'error',
-                    title: 'Oops...',
-                    text: 'You need to be logged in!',
-                });
-                throw new AuthenticationError('You need to be logged in!');
+            if (!context.user) {
+                throw new AuthenticationError("You need to be logged in!");
             }
+
             const updatedUser = await User.findByIdAndUpdate(
-                { _id: context.user._id },
+                context.user._id,
                 { $addToSet: { savedBooks: book } },
-                { new: true, runValidators: true }
+                { new: true }
             );
+
             return updatedUser;
         },
         removeBook: async (_, { bookId }, context) => {
-            if (context.user) {
-                Swal.fire({
-                    icon: 'error',
-                    title: 'Oops...',
-                    text: 'You need to be logged in!',
-                });
-                throw new AuthenticationError('You need to be logged in!');
+            if (!context.user) {
+                throw new AuthenticationError("You need to be logged in!");
             }
+
             const updatedUser = await User.findByIdAndUpdate(
-                { _id: context.user._id },
-                { $pull: { savedBooks: { _id: bookId } } },
-                { new: true, runValidators: true }
+                context.user._id,
+                { $pull: { savedBooks: { bookId: bookId } } },
+                { new: true }
             );
+
             return updatedUser;
         },
-    }
+    },
 };
 
 module.exports = resolvers;
