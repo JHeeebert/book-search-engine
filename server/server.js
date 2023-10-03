0e35e48c83c83529d8dbe4eae706ee6046a07985
Joshua Hebert <jhebertcocktails@gmail.com>
2023-10-03 16:35:55 -0400

    updated dependencies and ran npm install

--- server/server.js
+++ server/server.js
@@ -1,40 +1,38 @@
-// Import dependencies
 const express = require('express');
 const path = require('path');
-// Import ApolloServer, Middleware, typeDefs and resolvers
 const { ApolloServer } = require('apollo-server-express');
-const { authMiddleware } = require('./utils/auth');
 const { typeDefs, resolvers } = require('./schemas');
-// import connection to database and create a new Apollo server
+const { authMiddleware } = require('./utils/auth');
 const db = require('./config/connection');
-const app = express();
+// Load environment variables from .env file
+require('dotenv').config();
 const PORT = process.env.PORT || 3001;
-
-// Create a new Apollo server and pass in our schema data
+const _dirname = path.dirname("");
+const pathToFile = path.join(_dirname, '../client/build');
+const app = express();
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: authMiddleware,
+  context: authMiddleware, // Attach authentication middleware
 });
-// Apply our middleware to the apollo server
 server.applyMiddleware({ app });
-// Tells the app to use express.json and express.urlencoded to parse incoming data
+// Middleware to handle URL-encoded and JSON request bodies
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// Serve up static assets
-const _dirname = path.dirname("");
-const pathToFile = path.join(_dirname, '../client/build');
+// Serve static files from the React build folder
 app.use(express.static(pathToFile));
-// if we're in production, serve client/build as static assets
+// Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
-// wildcard GET route for the server
+// Wildcard GET route to serve React app
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
-// Start the server once the database connection is open
+// Connect to the database and start the server
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  app.listen(PORT, () => {
+    console.log(`🌍 Now listening on localhost:${PORT}`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
 });
