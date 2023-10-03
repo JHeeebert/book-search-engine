a6cba2a611d01d6b500bf6cb49f3c436945e7de7
Joshua Hebert <jhebertcocktails@gmail.com>
2023-10-03 15:27:25 -0400

    added sweetalerts2 to the auth.js

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -1,5 +1,7 @@
+/* eslint-disable import/no-anonymous-default-export */
 // use this to decode a token and get the user's information out of it
-import decode from 'jwt-decode';
+import decode from "jwt-decode";
+import Swal from "sweetalert2";
 
 // create a new class to instantiate for a user
 class AuthService {
@@ -29,20 +31,30 @@ class AuthService {
 
   getToken() {
     // Retrieves the user token from localStorage
-    return localStorage.getItem('id_token');
+    return localStorage.getItem("id_token");
   }
 
   login(idToken) {
-    // Saves user token to localStorage
-    localStorage.setItem('id_token', idToken);
-    window.location.assign('/');
-  }
+    localStorage.setItem("id_token", idToken);
+    // Display SweetAlert for successful login
+    Swal.fire({
+      icon: "success",
+      title: "Login Successful",
+      text: "You have successfully logged in.",
+    });
 
+    window.location.assign("/");
+  }
   logout() {
-    // Clear user token and profile data from localStorage
-    localStorage.removeItem('id_token');
-    // this will reload the page and reset the state of the application
-    window.location.assign('/');
+    localStorage.removeItem("id_token");
+    // Display SweetAlert for successful logout
+    Swal.fire({
+      icon: "success",
+      title: "Logout Successful",
+      text: "You have successfully logged out.",
+    });
+
+    window.location.assign("/");
   }
 }
 
