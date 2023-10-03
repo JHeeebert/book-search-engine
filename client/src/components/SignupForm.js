9ffc51470ac14bfb4ffc538630f54264be443f33
Joshua Hebert <jhebertcocktails@gmail.com>
2023-10-03 15:09:51 -0400

    added sweet alerts to saved books and search books also formatted signupform.js

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,94 +1,62 @@
 import React, { useState } from "react";
-
 import { Form, Button, Alert } from "react-bootstrap";
-
 import Swal from "sweetalert2";
-
 import "sweetalert2/dist/sweetalert2.min.css";
-
 import Auth from "../utils/auth";
-
 import { useMutation } from "@apollo/react-hooks";
-
 import { ADD_USER } from "../utils/mutations";
 
 const SignupForm = () => {
   const [userFormData, setUserFormData] = useState({
     username: "",
-
     email: "",
-
     password: "",
   });
-
   const [validated, setValidated] = useState(false);
-
   const [showAlert, setShowAlert] = useState(false);
-
   const [createUser] = useMutation(ADD_USER);
-
   const handleInputChange = (event) => {
     const { name, value } = event.target;
-
     setUserFormData({ ...userFormData, [name]: value });
   };
-
   const handleFormSubmit = async (event) => {
     event.preventDefault();
-
     const form = event.currentTarget;
-
     if (form.checkValidity() === false) {
       event.stopPropagation();
-
       setValidated(true); // Show validation feedback
-
       return;
     }
-
     try {
       const { data } = await createUser({
         variables: { ...userFormData },
       });
-
       if (data && data.addUser && data.addUser.token) {
         Auth.login(data.addUser.token);
       } else {
         console.error("Unexpected response from server:", data);
-
         Swal.fire({
           icon: "error",
-
           title: "Oops...",
-
           text: "Something went wrong with your signup!",
         });
       }
     } catch (err) {
       console.error("Error during mutation:", err);
-
       Swal.fire({
         icon: "error",
-
         title: "Oops...",
-
         text: err.message || "Something went wrong with your signup!",
       });
     }
-
     // Clear the form after submission
-
     setUserFormData({
       username: "",
-
       email: "",
-
       password: "",
     });
-
     setValidated(false);
   };
-
   return (
     <>
       <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,74 +1,64 @@
-import React, { useState, useEffect } from 'react';
-import {
-  Container,
-  Card,
-  Button,
-  Row,
-  Col
-} from 'react-bootstrap';
-
-import { getMe, deleteBook } from '../utils/API';
-import Auth from '../utils/auth';
-import { removeBookId } from '../utils/localStorage';
+import React, { useState, useEffect } from "react";
+import { Container, Card, Button, Row, Col } from "react-bootstrap";
+import { getMe, deleteBook } from "../utils/API";
+import Auth from "../utils/auth";
+import { removeBookId } from "../utils/localStorage";
+import Swal from "sweetalert2";
 
 const SavedBooks = () => {
   const [userData, setUserData] = useState({});
-
-  // use this to determine if `useEffect()` hook needs to run again
   const userDataLength = Object.keys(userData).length;
-
   useEffect(() => {
     const getUserData = async () => {
       try {
         const token = Auth.loggedIn() ? Auth.getToken() : null;
-
         if (!token) {
           return false;
         }
-
         const response = await getMe(token);
-
         if (!response.ok) {
-          throw new Error('something went wrong!');
+          throw new Error("Something went wrong!");
         }
-
         const user = await response.json();
         setUserData(user);
       } catch (err) {
         console.error(err);
       }
     };
-
     getUserData();
   }, [userDataLength]);
-
-  // create function that accepts the book's mongo _id value as param and deletes the book from the database
   const handleDeleteBook = async (bookId) => {
     const token = Auth.loggedIn() ? Auth.getToken() : null;
-
     if (!token) {
       return false;
     }
-
     try {
       const response = await deleteBook(bookId, token);
       if (!response.ok) {
-        throw new Error('something went wrong!');
+        throw new Error("Something went wrong!");
       }
       const updatedUser = await response.json();
       setUserData(updatedUser);
-      // upon success, remove book's id from localStorage
       removeBookId(bookId);
+      // Display SweetAlert for success
+      Swal.fire({
+        icon: "success",
+        title: "Book Deleted",
+        text: "The book has been successfully deleted.",
+      });
     } catch (err) {
       console.error(err);
+      // Display SweetAlert for error
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Something went wrong!",
+      });
     }
   };
-
-  // if data isn't here yet, say so
   if (!userDataLength) {
     return <h2>LOADING...</h2>;
   }
-
   return (
     <>
       <div fluid className="text-light bg-dark p-5">
@@ -76,30 +66,41 @@ const SavedBooks = () => {
           <h1>Viewing saved books!</h1>
         </Container>
       </div>
+
       <Container>
-        <h2 className='pt-5'>
+        <h2 className="pt-5">
           {userData.savedBooks.length
-            ? `Viewing ${userData.savedBooks.length} saved ${userData.savedBooks.length === 1 ? 'book' : 'books'}:`
-            : 'You have no saved books!'}
+            ? `Viewing ${userData.savedBooks.length} saved ${userData.savedBooks.length === 1 ? "book" : "books"
+            }:`
+            : "You have no saved books!"}
         </h2>
+
         <Row>
-          {userData.savedBooks.map((book) => {
-            return (
-              <Col md="4">
-                <Card key={book.bookId} border='dark'>
-                  {book.image ? <Card.Img src={book.image} alt={`The cover for ${book.title}`} variant='top' /> : null}
-                  <Card.Body>
-                    <Card.Title>{book.title}</Card.Title>
-                    <p className='small'>Authors: {book.authors}</p>
-                    <Card.Text>{book.description}</Card.Text>
-                    <Button className='btn-block btn-danger' onClick={() => handleDeleteBook(book.bookId)}>
-                      Delete this Book!
-                    </Button>
-                  </Card.Body>
-                </Card>
-              </Col>
-            );
-          })}
+          {userData.savedBooks.map((book, index) => (
+            <Col key={`${book.bookId}-${index}`} md="4">
+              <Card border="dark">
+                {book.image ? (
+                  <Card.Img
+                    src={book.image}
+                    alt={`The cover for ${book.title}`}
+                    variant="top"
+                  />
+                ) : null}
+
+                <Card.Body>
+                  <Card.Title>{book.title}</Card.Title>
+                  <p className="small">Authors: {book.authors}</p>
+                  <Card.Text>{book.description}</Card.Text>
+                  <Button
+                    className="btn-block btn-danger"
+                    onClick={() => handleDeleteBook(book.bookId)}
+                  >
+                    Delete this Book!
+                  </Button>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
         </Row>
       </Container>
     </>
